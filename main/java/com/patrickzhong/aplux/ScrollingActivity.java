fe665708a8ff14cdd45d5ac228ed3b02dc362344
Patrick Zhong <18PatZ@students.harker.org>
2017-03-17 21:49:27 -0700

    Get Location, Request Permission, Firebase Write and Read

--- main/java/com/patrickzhong/aplux/ScrollingActivity.java
+++ main/java/com/patrickzhong/aplux/ScrollingActivity.java
@@ -1,13 +1,28 @@
 package com.patrickzhong.aplux;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -19,13 +34,107 @@ public class ScrollingActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final AppCompatActivity act = this;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+
+
+                if(ContextCompat.checkSelfPermission(act, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    System.out.println("has access");
+                    printLoc();
+                }
+                else {
+                    System.out.println("requesting access");
+                    ActivityCompat.requestPermissions(act,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            5);
+                }
+
+
             }
         });
+
+
+    }
+
+    public void printLoc(){
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        database.setPersistenceEnabled(true);
+        final DatabaseReference ref = database.getReference();
+        System.out.println(ref.toString());
+        ref.child("Hello").setValue("World!");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println("Data: "+dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+        //myRef.setValue("Hello, World!");
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                String str = location.getLongitude()+" "+location.getLatitude()+" "+location.getAltitude();
+                System.out.println(str);
+                ref.child("Location").setValue(str);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        //String locationProvider = LocationManager.NETWORK_PROVIDER;
+        //Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            //System.out.println(lastKnownLocation.toString());
+            System.out.println("looking for loc");
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 5: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    printLoc();
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
