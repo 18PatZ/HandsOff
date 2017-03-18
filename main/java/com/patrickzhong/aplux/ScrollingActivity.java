655a6f09dfa2d68d8d0c55bf20f2ff8f24178349
Patrick Zhong <18PatZ@students.harker.org>
2017-03-18 07:05:12 -0700

    Report Creation and Displaying, Attempt at Styling

--- main/java/com/patrickzhong/aplux/ScrollingActivity.java
+++ main/java/com/patrickzhong/aplux/ScrollingActivity.java
@@ -1,6 +1,9 @@
 package com.patrickzhong.aplux;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,15 +12,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class ScrollingActivity extends AppCompatActivity {
 
     static FirebaseDatabase database;
     static DatabaseReference ref;
+
+
+    public static ScrollingActivity instance;
+    public boolean requesting = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,97 +40,77 @@ public class ScrollingActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        if(database == null) {
+            database = FirebaseDatabase.getInstance();
+            database.setPersistenceEnabled(true);
+            ref = database.getReference();
+        }
 
-        database = FirebaseDatabase.getInstance();
-        database.setPersistenceEnabled(true);
-        ref = database.getReference();
+        instance = this;
 
         final AppCompatActivity act = this;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action!", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Changing to ReportActivity...", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-
-                Util.go(act);
-                /*
-
-
-                if(ContextCompat.checkSelfPermission(act, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    System.out.println("has access");
-                    printLoc();
-                }
-                else {
-                    System.out.println("requesting access");
-                    ActivityCompat.requestPermissions(act,
-                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                            5);
-                }*/
-
-
+                startActivity(new Intent(ScrollingActivity.this, ReportActivity.class));
             }
         });
 
 
+
+        //Util.go(act);
+
+        //LinearLayout ll = (LinearLayout) findViewById(R.id.linearLayout);
+        Button b = (Button) findViewById(R.id.buttonOne);
+        b.setText("Test");
+
+        if(Util.lastLoc == null) {
+            requesting = true;
+            Util.requestLoc(this);
+        }
+        else
+            handle(Util.lastLoc);
     }
 
-    public void printLoc(){
-
-        /*FirebaseDatabase database = FirebaseDatabase.getInstance();
-        database.setPersistenceEnabled(true);
-        final DatabaseReference ref = database.getReference();
-        System.out.println(ref.toString());
-        ref.child("Hello").setValue("World!");
-
-        ref.addValueEventListener(new ValueEventListener() {
+    @TargetApi(24)
+    public static void handle(final Location location){
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                System.out.println("Data: "+dataSnapshot.getValue());
+            public void onDataChange(DataSnapshot snapshot) {
+                LinearLayout ll = (LinearLayout) instance.findViewById(R.id.linearLayout);
+                ll.removeAllViews();
+                for(DataSnapshot each : snapshot.getChildren()){
+                    String desc = each.child("Description").getValue(String.class);
+                    String id = each.getKey();
+                    String loc = each.child("Location").getValue(String.class);
+
+                    String[] arr = loc.split(" ");
+                    float[] results = new float[3];
+                    Location.distanceBetween(location.getLatitude(), location.getLongitude(), Double.parseDouble(arr[0]), Double.parseDouble(arr[1]), results);
+                    double dist = results[0];
+
+                    TextView bb = new TextView(instance);
+
+                    bb.setText(id+"\n"+desc+"\n"+"Distance: "+dist+" meters.\n");
+                    bb.setTextAppearance(R.style.ButtonTheme);
+                    ll.addView(bb);
+                }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
+
             }
         });
-        //myRef.setValue("Hello, World!");
-
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-        LocationListener locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                // Called when a new location is found by the network location provider.
-                Util.addReport("1", "Filler description.");
-                String str = location.getLongitude()+" "+location.getLatitude()+" "+location.getAltitude();
-                System.out.println(str);
-                ref.child("Location").setValue(str);
-            }
-
-            public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-            public void onProviderEnabled(String provider) {}
-
-            public void onProviderDisabled(String provider) {}
-        };
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-        //String locationProvider = LocationManager.NETWORK_PROVIDER;
-        //Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
-
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            //System.out.println(lastKnownLocation.toString());
-            System.out.println("looking for loc");
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        }*/
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        if(requestCode == 5){
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                Util.addReport(this);
-        }
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            //Util.go(this, requestCode);
+            Util.requestLoc(this);
     }
 
     @Override
--- main/java/com/patrickzhong/aplux/Util.java
+++ main/java/com/patrickzhong/aplux/Util.java
@@ -1,6 +1,7 @@
 package com.patrickzhong.aplux;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -10,31 +11,61 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
-import static com.patrickzhong.aplux.ScrollingActivity.ref;
-
 /**
  * Created by patrickzhong on 3/17/17.
  */
 
 public class Util {
 
+    public static String id;
+    public static String desc;
+    public static boolean reportRequesting = false;
+    public static LocationListener locationListener;
+    public static Location lastLoc;
+
     public static void pushLocation(String id, String desc, Location location){
         ScrollingActivity.ref.child(id).child("Description").setValue(desc);
         ScrollingActivity.ref.child(id).child("Location").setValue(serialize(location));
+        ReportActivity.instance.showProgress(false);
+        ReportActivity.instance.startActivity(new Intent(ReportActivity.instance, ScrollingActivity.class));
     }
 
     public static String serialize(Location location){
         return location.getLatitude() + " " + location.getLongitude() + " " + location.getAltitude();
     }
 
-    public static void addReport(AppCompatActivity cont){
+    public static void requestLoc(AppCompatActivity cont) {
+        if (ContextCompat.checkSelfPermission(cont, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            setLoc(cont);
+        else
+            ActivityCompat.requestPermissions(cont, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+    }
 
-        LocationManager locationManager = (LocationManager) cont.getSystemService(Context.LOCATION_SERVICE);
+    public static void preparePush(String idN, String descN){
+        id = idN;
+        desc = descN;
+    }
 
-        LocationListener locationListener = new LocationListener() {
+    private static void setLoc(final AppCompatActivity cont){
+
+        if(locationListener != null)
+            return;
+
+        final LocationManager locationManager = (LocationManager) cont.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
-                int r = (int) (Math.random() * 100);
-                Util.pushLocation(""+r, "Filler description: "+r, location);
+                System.out.println("LOCATION RECIEVED");
+                if(ScrollingActivity.instance.requesting) {
+                    ScrollingActivity.handle(location);
+                    ScrollingActivity.instance.requesting = false;
+                }
+                else if(reportRequesting) {
+                    Util.pushLocation(id, desc, location);
+                    reportRequesting = false;
+                }
+
+                lastLoc = location;
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {}
@@ -47,12 +78,4 @@ public class Util {
         if(ContextCompat.checkSelfPermission(cont, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
-
-    public static void go(AppCompatActivity cont){
-        if(ContextCompat.checkSelfPermission(cont, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-            addReport(cont);
-        else
-            ActivityCompat.requestPermissions(cont, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 5);
-    }
-
 }
